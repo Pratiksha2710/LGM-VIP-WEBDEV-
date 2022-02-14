b65966593d1f2b094422a99b06523fa2d72de587
Pratiksha Gote <gotepratiksha07.com>
2022-02-14 17:49:13 +0530

    task 1 added

---
+++ LGM-TASK1/script.js
@@ -0,0 +1,30 @@
+
+document.querySelector('#add').onclick = function() {
+if(document.querySelector('#newtask input').value.length == 0){
+    alert("Please Enter a Task")
+}
+else{
+
+    document.querySelector('#tasks').innerHTML += 
+    `
+    <div class="task">
+    <span id="taskname">
+    ${document.querySelector('#newtask input').value}
+    </span>
+
+    <button class="delete">
+     <img src="https://img.icons8.com/material/344/filled-trash.png"/>
+    </button>
+    </div>
+    `;
+    
+    var current_tasks = document.querySelectorAll(".delete");
+    for(var i=0; i<current_tasks.length; i++){
+        current_tasks[i].onclick = function(){
+            this.parentNode.remove();
+        }
+    }
+}
+
+}
+
